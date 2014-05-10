dd32ca99cff1660494ef792fe890a8a962225286
Andy Liu <andyhome.liu@gmail.com>
2014-05-10 20:57:04 +0900

    Add web module

---
+++ web/src/main/java/org/asuki/web/listener/CustomPhaseListener.java
@@ -0,0 +1,29 @@
+package org.asuki.web.listener;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class CustomPhaseListener implements PhaseListener {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void afterPhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
