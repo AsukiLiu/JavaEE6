74448f381c93760df985973ee870b511db97cf4c
Andy Liu <andyhome.liu@gmail.com>
2014-06-13 22:28:43 +0900

    Add Providers

--- demo-webservice/src/main/java/org/asuki/webservice/rs/resource/DemoResource.java
+++ demo-webservice/src/main/java/org/asuki/webservice/rs/resource/DemoResource.java
@@ -1,13 +1,19 @@
 package org.asuki.webservice.rs.resource;
 
+import static com.google.common.collect.Maps.newLinkedHashMap;
 import static java.lang.String.format;
+import static java.util.Arrays.asList;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.MatrixParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -15,6 +21,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 
+//http://localhost:8080/demo-web/rs/demo
 @Path("/demo")
 @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
 public class DemoResource extends BaseResource {
@@ -29,7 +36,7 @@ public class DemoResource extends BaseResource {
         throw new IllegalArgumentException("IllegalArgumentException throwed");
     }
 
-    // http://localhost:8080/demo-web/rs/demo/user/a01/xxx@gmail.com/090-9999-9999
+    // user/a01/xxx@gmail.com/090-9999-9999
     @Path("user/{id: [a-zA-Z][a-zA-Z0-9]*}/{mail}/{phone}")
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -48,7 +55,7 @@ public class DemoResource extends BaseResource {
         return user.toString();
     }
 
-    // http://localhost:8080/demo-web/rs/demo/position;latitude=37.12;longitude=165.26
+    // position;latitude=37.12;longitude=165.26
     // @formatter:off
     @Path("/position")
     @GET
@@ -65,5 +72,47 @@ public class DemoResource extends BaseResource {
                 longitude == 0.0 ? "": (longitude > 0.0 ? "E" : "W"));
     }
     // @formatter:on
+
+    @GET
+    @Path("csv")
+    @Produces("text/csv")
+    public String[][] getCsv() {
+
+        String[] row = { "a", "bb", "ccc" };
+        String[][] table = { row, row, row };
+
+        return table;
+    }
+
+    // @formatter:off
+    /*
+    [
+        {
+            "Key1": "Value1",
+            "Key2": "Value2",
+            "Key3": "Value3"
+        },
+        {
+            "KeyA": "ValueA",
+            "KeyB": "ValueB",
+            "KeyC": "ValueC"
+        }
+    ]
+     */
+    // @formatter:on
+    @POST
+    @Path("list")
+    @Consumes({ APPLICATION_JSON })
+    @Produces({ APPLICATION_JSON })
+    public List<Map<String, String>> postList(List<Map<String, String>> input) {
+
+        log.info(input.toString());
+
+        Map<String, String> map = newLinkedHashMap();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+
+        return asList(map, map);
+    }
 
 }
