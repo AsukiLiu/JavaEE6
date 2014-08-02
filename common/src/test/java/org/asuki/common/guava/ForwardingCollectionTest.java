bf8ed89d6e3713f4fb2f975c9d7363a8d4ac6f96
Andy Liu <andyhome.liu@gmail.com>
2014-08-02 14:06:02 +0900

    Guava forwarding collection

--- common/src/test/java/org/asuki/common/guava/ForwardingCollectionTest.java
+++ common/src/test/java/org/asuki/common/guava/ForwardingCollectionTest.java
@@ -1,17 +1,23 @@
 package org.asuki.common.guava;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.primitives.Ints.asList;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 import java.util.List;
 
+import org.asuki.common.guava.forwarding.CounterMultiset;
+import org.asuki.common.guava.forwarding.MaxMultiset;
+import org.asuki.common.guava.forwarding.MinMultiset;
 import org.asuki.common.guava.forwarding.MovableList;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.google.common.base.Function;
 import com.google.common.base.Joiner;
+import com.google.common.collect.LinkedHashMultiset;
+import com.google.common.collect.Multiset;
 
 public class ForwardingCollectionTest {
 
@@ -20,7 +26,7 @@ public class ForwardingCollectionTest {
             String expectString) {
 
         List<String> array = newArrayList("1", "2", "3", "4", "5");
-        MovableList<String> list = new MovableList<String>(array);
+        MovableList<String> list = new MovableList<>(array);
 
         move.apply(list);
 
@@ -63,6 +69,64 @@ public class ForwardingCollectionTest {
             }, 
         };
         // @formatter:on
+    }
+
+    @Test
+    public void testOnceDecoration() {
+        Multiset<String> multiset = LinkedHashMultiset.create();
+        CounterMultiset<String> counter = new CounterMultiset<>(multiset);
+
+        for (int j = 0; j < 3; j++) {
+            for (int i = 0; i < 3; i++) {
+                counter.add("element" + i);
+            }
+        }
+
+        List<Integer> sizes = asList(multiset.size(), counter.size());
+
+        for (int size : sizes) {
+            assertThat(size, is(3 * 3));
+        }
+
+        assertThat(multiset.elementSet().toString(),
+                is("[element0, element1, element2]"));
+
+        counter.clear();
+        sizes = asList(multiset.size(), counter.size());
+        for (int size : sizes) {
+            assertThat(size, is(0));
+        }
+    }
+
+    @Test
+    public void testMoreDecorations() {
+        final String target = "element";
+        Multiset<String> multiset = LinkedHashMultiset.create();
+        CounterMultiset<String> counter = new CounterMultiset<>(multiset);
+        MaxMultiset<String> max = new MaxMultiset<>(counter);
+        MinMultiset<String> min = new MinMultiset<>(max);
+
+        for (int i = 1; i < 4; i++) {
+            min.add(target, i + 1);
+        }
+
+        List<Integer> sizes = asList(multiset.size(), counter.size(),
+                max.size(), min.size(), counter.count(target),
+                max.count(target), min.count(target));
+
+        for (int size : sizes) {
+            assertThat(size, is(3 * 3));
+        }
+
+        assertThat(counter.countCounter(target), is(4 - 1));
+        assertThat(max.countMax(target), is(3 + 1));
+        assertThat(min.countMin(target), is(1 + 1));
+
+        min.clear();
+        sizes = asList(multiset.size(), counter.size(), max.size(), min.size());
+        for (int size : sizes) {
+            assertThat(size, is(0));
+        }
     }
 
 }
